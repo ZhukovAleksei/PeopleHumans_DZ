3b10ee7cfe265c033a31336c1b44ba07a63b2b58
Aleksey Zhukov <jhukov2001@mail.ru>
2025-02-28 19:17:39 +0300

    Second commit

--- src/main/kotlin/Task.kt
+++ src/main/kotlin/Task.kt
@@ -1,6 +1,11 @@
-fun main(){
-    var likes = 471
-    
-    val likesPeople = if(likes == 1 || likes % 10 == 1 ) "Понравилось $likes человеку" else "Понравилось $likes людям"
+fun main() {
+    var likes = 511
+    val likesPeople = if (likes % 100 == 11 || likes == 11) {
+        "Понравилось $likes людям"
+    } else if (likes == 1 || likes % 10 == 1) {
+        "Понравилось $likes человеку"
+    } else {
+        "Понравилось $likes людям"
+    }
     println(likesPeople)
 }
